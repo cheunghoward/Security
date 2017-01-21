aca62367d043228774abc59ebb47c54a5f85f8ef
Howard <howardc@ccs.neu.edu>
2017-01-20 23:54:46 -0500

    asdf

--- 1/ChatClient.py
+++ 1/ChatClient.py
@@ -6,26 +6,42 @@ import select
 
 MAX_UDP_PACKET_SIZE = 65507
 
+# Parse user input
 parser = argparse.ArgumentParser()
-parser.add_argument("-sp", type=int)
-parser.add_argument("-sip", type=str)
-parser.add_argument("-u", type=str)
-
+parser.add_argument("-sip", type=str, required=True)
+parser.add_argument("-sp", type=int, required=True)
+parser.add_argument("-u", type=str, required=True)
 args = parser.parse_args(sys.argv[1:])
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+SERVER = (args.sip, args.sp)
+USERNAME = args.u
 
-send_message = {"type": "LIST"}
-sign_in_message = {"type": "SIGN-IN", "message": args.u}
+# Static message types
+list_message = {"type": "LIST"}
+sign_in_message = {"type": "SIGN-IN", "message": USERNAME}
+
+# Initialize UDP socket and sign in
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendto(json.dumps(sign_in_message), SERVER)
 
 while True:
 
     # Use select method to know when a socket is ready to be read
     ready, ignore, ignore2 = select.select([s], [], [], 0.1)
+    user_input_socket, ignore, ignore2 = select.select([sys.stdin], [], [], 0.1)
 
-    for x in ready:
-        print x.recv(MAX_UDP_PACKET_SIZE)
+    for sock in ready:
+        print sock.recv(MAX_UDP_PACKET_SIZE)
 
-    user_input = raw_input("> ")
-    if user_input.split()[0] == "list":
-        s.sendto(json.dumps(send_message), ("10.0.0.70", 9090))
+    if user_input_socket:
+        user_input = user_input_socket.recv(MAX_UDP_PACKET_SIZE)
+        if user_input.split()[0] == "list":
+            s.sendto(json.dumps(list_message), SERVER)
+        elif user_input.split()[0] == "send":
+            s.sendto(json.dumps({"type": "MESSAGE", "user": user_input.split()[1]}), SERVER)
+            # Need to receive message again
+            ready, ignore, ignore2 = select.select([s], [], [], 0.1)
+            for sock in ready:
+                decoded = json.loads(sock.recv(MAX_UDP_PACKET_SIZE))
+                print decoded
+            s.sendto(" ".join(user_input.split()[2:]), (decoded["host"], decoded["port"]))
--- 1/ChatServer.py
+++ 1/ChatServer.py
@@ -8,7 +8,7 @@ MAX_UDP_PACKET_SIZE = 65507
 clients = {}
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-sp", type=int)
+parser.add_argument("-sp", type=int, required=True)
 args = parser.parse_args(sys.argv[1:])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,13 +17,15 @@ print "Server Initialized..."
 
 while True:
     (data, address) = s.recvfrom(MAX_UDP_PACKET_SIZE)
-    # For future reference: the protocol must be followed exactly
     decoded = json.loads(data)
 
     if decoded["type"] == "LIST":
         s.sendto("Signed In Users: " + ", ".join(clients.keys()), address)
-    elif decoded["type"] == "SIGN-ON":
+    elif decoded["type"] == "SIGN-IN":
         clients[decoded["message"]] = address
+    elif decoded["type"] == "MESSAGE":
+        (host, port) = clients[decoded["user"]]
+        s.sendto(json.dumps({"host": host, "port": port}), address)
     else:
         print "Cannot decode"
 
