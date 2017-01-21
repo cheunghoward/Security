82af944f36afccddb8e511c36579009c5ec27e6c
Howard <howardc@ccs.neu.edu>
2017-01-21 00:55:06 -0500

    Done

--- 1/ChatClient.py
+++ 1/ChatClient.py
@@ -3,6 +3,8 @@ import sys
 import json
 import socket
 import select
+import Queue
+import threading
 
 MAX_UDP_PACKET_SIZE = 65507
 
@@ -24,17 +26,31 @@ sign_in_message = {"type": "SIGN-IN", "message": USERNAME}
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.sendto(json.dumps(sign_in_message), SERVER)
 
+input_queue = Queue.Queue()
+
+# Function used in threading to read stdin in a non-blocking manner
+def add_input():
+    while True:
+        input_queue.put(sys.stdin.readline())
+
+print "enter list or send:\n"
 while True:
 
     # Use select method to know when a socket is ready to be read
     ready, ignore, ignore2 = select.select([s], [], [], 0.1)
-    user_input_socket, ignore, ignore2 = select.select([sys.stdin], [], [], 0.1)
+
+    # Spawn thread for reading stdin
+    # This is required because we may receive messages while waiting for user input
+    input_thread = threading.Thread(target=add_input)
+    input_thread.daemon = True
+    input_thread.start()
 
     for sock in ready:
-        print sock.recv(MAX_UDP_PACKET_SIZE)
+        (message, (host, port)) = sock.recvfrom(MAX_UDP_PACKET_SIZE)
+        print "<From " + host + ":" + str(port) + "> " + message
 
-    if user_input_socket:
-        user_input = user_input_socket.recv(MAX_UDP_PACKET_SIZE)
+    if not input_queue.empty():
+        user_input = input_queue.get()
         if user_input.split()[0] == "list":
             s.sendto(json.dumps(list_message), SERVER)
         elif user_input.split()[0] == "send":
@@ -43,5 +59,4 @@ while True:
             ready, ignore, ignore2 = select.select([s], [], [], 0.1)
             for sock in ready:
                 decoded = json.loads(sock.recv(MAX_UDP_PACKET_SIZE))
-                print decoded
             s.sendto(" ".join(user_input.split()[2:]), (decoded["host"], decoded["port"]))
